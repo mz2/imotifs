fe1d54841e755060ed615faca20beed949134fdd
Matias Piipari <matias.piipari@gmail.com>
2010-01-06 16:56:20 +0000

    FASTA quicklook beginnings

---
+++ fasta-quicklook/FASTAParser.h
@@ -0,0 +1,16 @@
+//
+//  FASTAParser.h
+//  FASTAQuickLook
+//
+//  Created by Matias Piipari on 06/01/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface FASTAParser : NSObject {
+
+}
+
+@end
